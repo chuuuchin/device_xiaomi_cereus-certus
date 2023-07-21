d26878bda065a7ac0b48f93a285452fe5647f7ec
Sushrut1101 <guptasushrut@gmail.com>
2023-07-21 17:47:50 -0300

    cereus: Build the Gatekeeper HAL from Source
    Signed-off-by: SH4ND <miqueiasantos20@gmail.com>

--- device.mk
+++ device.mk
@@ -57,6 +57,11 @@ PRODUCT_PACKAGES += \
     libdrmclearkeyplugin \
     libmockdrmcryptoplugin
 
+# Gatekeeper
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-service \
+    android.hardware.gatekeeper@1.0-impl
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
